87a68f99ca50f5343b33f33605acae93907353fd
unknown <salimiyahya549@gmail.com>
2022-07-05 18:35:42 +0300

    add landing page

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,3 +1,106 @@
 <template>
-	<div>Wow This is awesome</div>
+	<v-col xs="11" sm="8" md="6">
+		<p class="text-justify">
+			Welcome to <span class="gaurdian">Gaurdian</span> :) We are commited to
+			make people feel safe about their credintials. We know it is hard. The
+			applications out their are complicated and often expensive to use. That is
+			why we came up with this password management application. Why choose
+			Gaurdian? The answer is:
+		</p>
+		<ul class="mb-6">
+			<li class="appear">IT IS SIMPLE</li>
+			<li class="appear">IT IS SECURE</li>
+			<li class="appear">IT IS MADE WITH ❤️</li>
+		</ul>
+		<v-btn
+			:class="{ left: toggle, right: !toggle }"
+			outlined
+			large
+			color="primary"
+			@click="handleClick"
+			@mouseenter="toggle = !toggle"
+			>Get started</v-btn
+		>
+	</v-col>
 </template>
+
+<script>
+import router from "@/router";
+import { mapGetters } from "vuex";
+export default {
+	data() {
+		return {
+			toggle: false,
+		};
+	},
+	methods: {
+		handleClick() {
+			router.push(this.loggedInUser ? "/home" : "/login");
+		},
+	},
+	computed: {
+		...mapGetters(["loggedInUser"]),
+	},
+};
+</script>
+
+<style scoped>
+.gaurdian {
+	text-decoration: wavy;
+	letter-spacing: 1.2px;
+	color: orange;
+}
+.left,
+.right {
+	transition: border-radius 0.3s linear;
+}
+.left:hover {
+	border-top-right-radius: 25px;
+	border-bottom-left-radius: 25px;
+}
+
+.right:hover {
+	border-top-left-radius: 25px;
+	border-bottom-right-radius: 25px;
+}
+.appear {
+	font-weight: 500;
+}
+@keyframes appear {
+	from {
+		opacity: 0;
+	}
+	to {
+		opacity: 1;
+	}
+}
+.appear {
+	animation: appear 2s;
+	list-style-type: none;
+	position: relative;
+}
+
+.appear::before {
+	content: "";
+	border: 1px solid blueviolet;
+	background-color: blueviolet;
+	width: 10px;
+	height: 10px;
+	position: absolute;
+	left: -23px;
+	top: 6px;
+	transform: rotate(45deg);
+}
+
+.appear::after {
+	content: "";
+	border: 1px solid blueviolet;
+	width: 15px;
+	height: 15px;
+	position: absolute;
+	left: -18.675px;
+	top: 10px;
+	border-right: none;
+	border-bottom: none;
+}
+</style>
