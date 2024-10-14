0d7fbcc8b68aeddfdbd2caf2974786c3fe9078f3
Yahya Salimi <yahya.salimi@eduhouse.fi>
2024-10-14 14:33:37 +0300

    point to all-in-one app instead of vartija backend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,9 @@
-const isDevMode = process.env.NODE_ENV === 'development'
-const useProdUrlAnyway = process.env.VUE_APP_USE_PROD_URL && process.env.VUE_APP_USE_PROD_URL === '1'
+const isDevMode = process.env.NODE_ENV === "development";
+const useProdUrlAnyway =
+  process.env.VUE_APP_USE_PROD_URL && process.env.VUE_APP_USE_PROD_URL === "1";
 
-const devUrl = 'http://localhost:5000'
-const prodUlr = 'https://vartija-backend.onrender.com'
+const devUrl = "http://localhost:5000";
+const prodUlr = "https://yahya-8csr.onrender.com";
 
-export const BACKEND_BASE_URL = isDevMode && !useProdUrlAnyway ? devUrl : prodUlr
+export const BACKEND_BASE_URL =
+  isDevMode && !useProdUrlAnyway ? devUrl : prodUlr;
